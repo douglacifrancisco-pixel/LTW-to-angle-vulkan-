39ef66adea4f72dd1f4705a8a3d9afeb7cf99a43
douglacifrancisco-pixel <douglacifrancisco@gmail.com>
2026-09-13 21:29:55 +0200

    Keep LWJGL EGL wrappers with LTO

--- ltw/src/main/tinywrapper/egl.c
+++ ltw/src/main/tinywrapper/egl.c
@@ -256,7 +256,7 @@ EGLBoolean eglMakeCurrent (EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGL
 
 
 // === EGL WRAPPERS FOR LWJGL/GLFW DLSYM COMPATIBILITY ===
-#define EGL_ATTR __attribute__((visibility("default")))
+#define EGL_ATTR __attribute__((visibility("default"), used))
 
 static EGLDisplay (*host_eglGetDisplay)(EGLNativeDisplayType) = NULL;
 EGL_ATTR EGLDisplay eglGetDisplay(EGLNativeDisplayType display_id) {
